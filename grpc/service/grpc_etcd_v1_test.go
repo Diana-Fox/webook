26dd63d87aaaf7347dcab04dec159b54f066bb65
diana <JXiuye@163.com>
2024-02-21 16:05:50 +0800

    #涉及续约机制的部分

--- grpc/service/grpc_etcd_v1_test.go
+++ grpc/service/grpc_etcd_v1_test.go
@@ -16,12 +16,12 @@ import (
 	"time"
 )
 
-type EtcdTestSuite struct {
+type EtcdTestSuiteV1 struct {
 	suite.Suite
 	client *etcdv3.Client
 }
 
-func (s *EtcdTestSuite) SetupSuite() {
+func (s *EtcdTestSuiteV1) SetupSuite() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	//这个是etcd
@@ -32,20 +32,40 @@ func (s *EtcdTestSuite) SetupSuite() {
 	require.NoError(s.T(), err)
 	s.client = client //初始化客户端
 }
-func (s *EtcdTestSuite) TestServer() {
+func (s *EtcdTestSuiteV1) TestServer() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	//过期时间
+	var ttl int64 = 60 //单位是s
+	leaseRes, err := s.client.Grant(ctx, ttl)
+	if err != nil {
+		return
+	}
 	//s.SetupSuite()
 	//target是当前服务注册的服务名，一个服务一个manager
 	manager, err := endpoints.NewManager(s.client, "service/user")
 	require.NoError(s.T(), err)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	//key是指实例的key,有instance id就是，没有就是ip
 	addr := "127.0.0.1:8090"
 	key := "service/user" + addr
-	manager.AddEndpoint(ctx, key, endpoints.Endpoint{
+	err = manager.AddEndpoint(ctx, key, endpoints.Endpoint{
 		Addr: addr,
 		//Metadata: UserServiceRPC{},//这个可以存放一些自定义的信息
-	})
+	}, etcdv3.WithLease(leaseRes.ID))
+	require.NoError(s.T(), err)
+	go func() {
+		//续约
+		ctxKl, cancelKl := context.WithTimeout(context.Background(), time.Second)
+		defer cancelKl()
+		//默认1/3时间开始续约
+		ch, errKl := s.client.KeepAlive(ctxKl, leaseRes.ID)
+		require.NoError(s.T(), errKl)
+		for kaResp := range ch {
+			s.T().Log(kaResp.String())
+		}
+	}()
 	server := grpc.NewServer()
 	defer func() {
 		server.GracefulStop() //优雅退出
@@ -62,7 +82,7 @@ func (s *EtcdTestSuite) TestServer() {
 		return
 	}
 }
-func (s *EtcdTestSuite) TestEtcdClient() {
+func (s *EtcdTestSuiteV1) TestEtcdClient() {
 	db, err := resolver.NewBuilder(s.client)
 	require.NoError(s.T(), err)
 	clientConn, err := grpc.Dial("etcd:///service/user",
@@ -77,6 +97,6 @@ func (s *EtcdTestSuite) TestEtcdClient() {
 	})
 	fmt.Println(login)
 }
-func TestEtcd(t *testing.T) {
+func TestEtcdV1(t *testing.T) {
 	suite.Run(t, new(EtcdTestSuite))
 }
